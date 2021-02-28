2e84ee8c16cffcc991f81db26f239ef717366ab7
EC2 Default User <ec2-user@ip-172-31-6-121.ec2.internal>
2021-02-28 04:20:13 +0000

    third

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,10 @@
 class Movie < ActiveRecord::Base
+    @@added_ratings = []
+    def self.add(rating)
+        @@added_ratings << rating
+    end
+    
+    def self.ratings
+        @@added_ratings
+    end
 end
