2ba6284bf18c965cbfc9a417800a920ca7cb172a
EC2 Default User <ec2-user@ip-172-31-6-121.ec2.internal>
2021-02-28 05:16:03 +0000

    fourth

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,5 @@
 class Movie < ActiveRecord::Base
+    @@all_ratings = ['G','PG','PG-13','R']
     @@added_ratings = []
     def self.add(rating)
         @@added_ratings << rating
@@ -6,5 +7,9 @@ class Movie < ActiveRecord::Base
     
     def self.ratings
         @@added_ratings
+    end
+    
+    def self.alls
+        @@all_ratings
     end
 end
